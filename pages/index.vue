12e421a8012effe7cb2a8d30b625f397850238f0
CJ Phua <cjphuaa@gmail.com>
2022-05-04 22:47:22 +0800

    update hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,45 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div>
+    <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+        <div id="menu-right">
+          <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
+            <li>About Us</li>
+            <li>Our Menu</li>
+            <li>Locations</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 pb-20 pt-10">
+        <img src="hero-burger.png" class="pr-8 mt-10" alt="" />
+        <div>
+          <h1 class="text-8xl text-white text-center font-lilita uppercase">
+            New Property In Town
+          </h1>
+          <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">
+            Luxury Condo
+          </h2>
+          <p class="text-white font-roboto font-light text-center">
+            Get the latest property in KL from luxury to the cheapest property in town
+          </p>
+        </div>
+      </div>
+    </div>
+    <div id="section-1" class="text-center bg-[url('/about.jpg')] pb-60 bg-cover">
+      <h1 class="text-6xl uppercase font-oswald text-center pt-20 px-24">
+        Property in KL is booming
+      </h1>
+      <p class="font-roboto font-light text-gray-500 text-center text-lg py-6 px-28">
+        Thinking when is the right time to invest in property.Now is the time check out
+        the lastest property in town
+      </p>
+      <button class="font-oswald uppercase bg-red-500 text-white py-3 px-8">
+        Explore Property
+      </button>
     </div>
   </div>
 </template>
